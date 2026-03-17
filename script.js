38cef73b84690a24cd7f874a244b1685727abdaf
luke-hol <lukeholpersonal@gmail.com>
2026-03-17 22:51:09 +0200

    First draft of my project site. Will probably end up being dick themed in some way, if I know myself well.

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.querySelector('.theme-toggle');
+    toggleBtn.addEventListener('click', () => {
+      document.body.classList.toggle('dark');
+      toggleBtn.textContent = document.body.classList.contains('dark') 
+        ? '☀️ Light Mode' : '🌙 Dark Mode';
+    });
